271aaa64bd8a86a43b482dd060fdbbec9ae325a2
Hoang Hai <hoanghaido2003@gmail.com>
2024-05-16 17:32:19 +0700

    update database

--- sql/Dump20240516.sql
+++ sql/Dump20240516.sql
@@ -16,6 +16,20 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `role_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `roles`
 --
 
@@ -26,6 +40,23 @@ INSERT INTO `roles` VALUES (1,'USER'),(2,'CREATOR'),(3,'EDITOR'),(4,'ADMIN');
 UNLOCK TABLES;
 
 --
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `enabled` bit(1) NOT NULL,
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `verification_code` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `users`
 --
 
@@ -34,6 +65,23 @@ LOCK TABLES `users` WRITE;
 INSERT INTO `users` VALUES (_binary '',1,'$2a$10$CyefWJgOyIocTjMqxjgi7esrJx7lEH/oD3baqwWNk.ZGUucbjngnC','patrick',NULL),(_binary '',2,'$2a$10$CyefWJgOyIocTjMqxjgi7esrJx7lEH/oD3baqwWNk.ZGUucbjngnC','alex',NULL),(_binary '',3,'$2a$10$CyefWJgOyIocTjMqxjgi7esrJx7lEH/oD3baqwWNk.ZGUucbjngnC','john',NULL),(_binary '',4,'$2a$10$CyefWJgOyIocTjMqxjgi7esrJx7lEH/oD3baqwWNk.ZGUucbjngnC','namhm',NULL),(_binary '',5,'$2a$10$CyefWJgOyIocTjMqxjgi7esrJx7lEH/oD3baqwWNk.ZGUucbjngnC','admin',NULL),(_binary '',6,'$2a$10$CyefWJgOyIocTjMqxjgi7esrJx7lEH/oD3baqwWNk.ZGUucbjngnC','trung',NULL),(_binary '',7,'$2a$10$CyefWJgOyIocTjMqxjgi7esrJx7lEH/oD3baqwWNk.ZGUucbjngnC','hai',NULL),(_binary '',8,'$2a$10$K9nkWimHt.89l.vZsmhjuO4ZhaeT0233BqF8PtZcrqoUuGDQ4QdRq','bach',NULL),(_binary '\0',9,'$2a$10$K9nkWimHt.89l.vZsmhjuO4ZhaeT0233BqF8PtZcrqoUuGDQ4QdRq','thai',NULL),(_binary '\0',10,'$2a$10$TZPCL6eBRfi9LTJth.jzTeWV9pGJ5EyXkPMoJXoy9yDpslp/tT8Ri','trinh','W6S2Kybz-W7OUdvQa2BJpgi0VHbs7M1_A9yHL5PVvXc4uS4kIqQOXtI4xMVrbMamHpjGhewC559ruvK2PEtl1Q'),(_binary '',15,'$2a$10$ICKOTNcEoaxI/NAf5xJPPOkWXlZYhoQxTqBV2h3Mt3ajs4qdj8ldC','hoanghaido2003@gmail.com','BNSutyVpF9HUBzROggBHrg7BQnMxWtn5xtB_Ng3dxOXbVR3nugVtkOtfN0uJ7M01GHZOiwozvmUxXqNN5t2r8g'),(_binary '',17,'$2a$10$flYGgWYfeF4YkW3Wjqn3sebnbBMXzf2BBfdB9zBoSwVHG28isnXDa','dang',NULL),(_binary '',23,'$2a$10$QBWYSaMebaKVCDQqAhvGY.uQtp4rLgPtxCgjmUIuBs2BqnOmltqgK','good',NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `users_roles`
+--
+
+DROP TABLE IF EXISTS `users_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users_roles` (
+  `role_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`role_id`,`user_id`),
+  KEY `FK2o0jvgh89lemvvo17cbqvdxaa` (`user_id`),
+  CONSTRAINT `FK2o0jvgh89lemvvo17cbqvdxaa` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `FKj6m8fwv7oqv74fcehir1a9ffy` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users_roles`
@@ -54,4 +102,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-16 15:59:38
+-- Dump completed on 2024-05-16 17:30:50
