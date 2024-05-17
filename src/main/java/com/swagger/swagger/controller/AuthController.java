25da70ffc58d756a55aad88f11690687424dbfcd
Hoang Hai <hoanghaido2003@gmail.com>
2024-05-17 13:46:43 +0700

    update

--- src/main/java/com/swagger/swagger/controller/AuthController.java
+++ src/main/java/com/swagger/swagger/controller/AuthController.java
@@ -19,32 +19,38 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.swagger.swagger.dto.UserDto;
+
 @RestController
 public class AuthController {
     BCryptPasswordEncoder encoder;
+
     public AuthController() {
         encoder = new BCryptPasswordEncoder();
     }
+
     @Autowired
     UserRepository userRepository;
     @Autowired
     AuthenticationManager authenticationManager;
     @Autowired
     JwtService jwtService;
+
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody UserDto userDto) {
         UserEntity exist = userRepository.findByUsername(userDto.getUsername());
-        if(exist==null){
-            return ResponseEntity.status(400).body(new UserException(400,"LOGIN_ERROR","Wrong Username"));
-        }else{
-            try{
+        if (exist == null) {
+            return ResponseEntity.status(400).body(new UserException(400, "LOGIN_ERROR", "Wrong Username"));
+        } else {
+            try {
                 Authentication authentication = authenticationManager.authenticate(
-                        new UsernamePasswordAuthenticationToken(userDto.getUsername(), userDto.getUsername() + userDto.getPassword()));
+                        new UsernamePasswordAuthenticationToken(userDto.getUsername(),
+                                userDto.getUsername() + userDto.getPassword()));
                 SecurityContextHolder.getContext().setAuthentication(authentication);
                 CustomUserDetail userDetails = (CustomUserDetail) authentication.getPrincipal();
-                return ResponseEntity.status(200).body(new UserException(200,"LOGIN_SUCCESS",jwtService.generateToken(userDetails)));
-            }catch (AuthenticationException ex){
-                return ResponseEntity.status(400).body(new UserException(400,"LOGIN_ERROR","Wrong Password"));
+                return ResponseEntity.status(200)
+                        .body(new UserException(200, "LOGIN_SUCCESS", jwtService.generateToken(userDetails)));
+            } catch (AuthenticationException ex) {
+                return ResponseEntity.status(400).body(new UserException(400, "LOGIN_ERROR", "Wrong Password"));
             }
         }
     }
@@ -55,20 +61,23 @@ public class AuthController {
         Pattern pattern = Pattern.compile(passwordRegex);
         UserEntity userEntity = new UserEntity();
         UserEntity exist = userRepository.findByUsername(userDto.getUsername());
-        if(exist!=null){
-            return ResponseEntity.status(400).body(new UserException(400,"REGISTER_ERROR","Username was already taken!"));
-        }else if (userDto.getUsername().isBlank()) {
-            return ResponseEntity.status(400).body(new UserException(400,"REGISTER_ERROR","Username is required"));
+        if (exist != null) {
+            return ResponseEntity.status(400)
+                    .body(new UserException(400, "REGISTER_ERROR", "Username was already taken!"));
+        } else if (userDto.getUsername() == null) {
+            return ResponseEntity.status(400).body(new UserException(400, "REGISTER_ERROR", "Username is required"));
         } else if (!pattern.matcher(userDto.getPassword()).matches()) {
-            return ResponseEntity.status(400).body(new UserException(400,"REGISTER_ERROR","Password's length must larger or equal 8 characters"));
-        }else {
+            return ResponseEntity.status(400).body(
+                    new UserException(400, "REGISTER_ERROR", "Password's length must larger or equal 8 characters"));
+        } else {
             userEntity.setEnabled(true);
-            userEntity.setPassword(encoder.encode(userDto.getUsername()+userDto.getPassword()));
+            userEntity.setPassword(encoder.encode(userDto.getUsername() + userDto.getPassword()));
             userEntity.setUsername(userDto.getUsername());
             userEntity.setVerificationCode(null);
             userEntity.setRoles(null);
             userRepository.save(userEntity);
-            return ResponseEntity.status(200).body(new UserException(200,"REGISTER_SUCCESSFULLY","Register Successfully!"));
+            return ResponseEntity.status(200)
+                    .body(new UserException(200, "REGISTER_SUCCESSFULLY", "Register Successfully!"));
         }
     }
 }
