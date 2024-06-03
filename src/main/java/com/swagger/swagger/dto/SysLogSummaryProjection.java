bbf8d5e624e8ca5a07bd7016746d38f34d3c88be
Hoang Hai <hoanghaido2003@gmail.com>
2024-06-03 20:22:59 +0700

    add SummaryProjection and SqlResultSetMapping

---
+++ src/main/java/com/swagger/swagger/dto/SysLogSummaryProjection.java
@@ -0,0 +1,6 @@
+package com.swagger.swagger.dto;
+
+public interface SysLogSummaryProjection {
+    String getDate();
+    Long getCount();
+}
--- src/main/java/com/swagger/swagger/entity/SysLogs.java
+++ src/main/java/com/swagger/swagger/entity/SysLogs.java
@@ -1,5 +1,7 @@
 package com.swagger.swagger.entity;
 
+import com.swagger.swagger.dto.SysLogDateDto;
+import com.swagger.swagger.dto.SysLogSummaryProjection;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,6 +14,27 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Data
 @Table(name = "SysLogs")
+@NamedNativeQuery(
+        name = "SysLog.countByDateRangeAndMethod",
+        query = """
+        SELECT FORMAT([CreatedTime], 'yyyy-MM') AS [date] 
+        , COUNT(*) AS [count] FROM SysLogs 
+        WHERE CreatedTime BETWEEN :startDate  and :endDate  
+        GROUP BY FORMAT([CreatedTime], 'yyyy-MM') 
+        ORDER BY [date]
+""",
+        resultSetMapping = "SysLogFilterResponseMapping"
+)
+@SqlResultSetMapping(
+        name = "SysLogFilterResponseMapping",
+        classes = @ConstructorResult(
+                targetClass = SysLogDateDto.class,
+                columns = {
+                        @ColumnResult(name="date", type = String.class),
+                        @ColumnResult(name = "count",type = Long.class)
+                }
+        )
+)
 public class SysLogs {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
